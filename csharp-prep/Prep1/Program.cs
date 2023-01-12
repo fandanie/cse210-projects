d11e2132c701028530f749c84de589c96d108e0b
fandanie <97694653+fandanie@users.noreply.github.com>
2023-01-12 12:28:13 -0500

    new

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your grade percentage: ");
+        string valueFromUser = Console.ReadLine();
+
+        int x = int.Parse(valueFromUser);
+
+        if (x >= 90)
+        {
+            Console.WriteLine('A');
+        }
+        else if (x >= 80 && x <= 89.9)
+        {
+            Console.WriteLine('B');
+        }
+        else if (x >= 70 && x <= 79.9)
+        {
+            Console.WriteLine('C');
+        }
+        else if (x >= 60 && x <= 69.9)
+        {
+            Console.WriteLine('D');
+        }
+        else if (x < 60)
+        {
+            Console.WriteLine('F');
+        }
+        
+        if (x >= 70)
+        {
+            Console.WriteLine("Congratulations");
+        }
+
     }
 }
